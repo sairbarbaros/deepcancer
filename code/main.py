10c4a485e46ccf0e91d01bb82508c71d89ab4e0d
Barbaros Şair <103073388+sairbarbaros@users.noreply.github.com>
2022-08-31 13:39:49 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -9,14 +9,14 @@ training_set = tf.keras.preprocessing.image_dataset_from_directory(path,
                                              shuffle=True,
                                              batch_size=64,
                                              image_size=(160,160),
-                                             validation_split=0.15,
+                                             validation_split=0.1,
                                              subset='training',
                                              seed=34)
 test_set = tf.keras.preprocessing.image_dataset_from_directory(path,
                                              shuffle=True,
                                              batch_size=64,
                                              image_size=(160,160),
-                                             validation_split=0.17,
+                                             validation_split=0.16,
                                              subset='validation',
                                              seed=34)
 
@@ -30,6 +30,7 @@ def data_augmenter():
     data_augmentation.add(tf.keras.layers.experimental.preprocessing.RandomFlip("vertical"))
     data_augmentation.add(tf.keras.layers.experimental.preprocessing.RandomRotation(0.34))
     data_augmentation.add(tf.keras.layers.experimental.preprocessing.RandomCrop(160,160))
+    data_augmentation.add(tf.keras.layers.experimental.preprocessing.RandomContrast(0.7, 1.2))
     
     return data_augmentation
 
@@ -55,7 +56,7 @@ def tumor_identifier(image_shape=(160,160), data_augmentation=data_augmenter()):
     x = tf.keras.layers.GlobalMaxPooling2D()(x) 
     x = tf.keras.layers.Dropout(0.34)(x)
         
-    outputs = tf.keras.layers.Dense(1)(x)
+    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.Model(inputs, outputs)
     
 
@@ -63,14 +64,43 @@ def tumor_identifier(image_shape=(160,160), data_augmentation=data_augmenter()):
 
 brain_model = tumor_identifier((160,160), data_augmenter())
 
-brain_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0034),
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              metrics=['acc'])
+brain_model = tumor_identifier((160,160), data_augmenter())
+
+brain_model.summary()
+
+brain_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=20, # how many epochs to wait before stopping
+    restore_best_weights=True,)
 
 history = brain_model.fit(
         training_set,
         validation_data=test_set,
-        epochs=150, 
+        epochs=300, 
         shuffle=True, 
         verbose=True,
-        )
+        callbacks=[early_stopping])
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, label='training acc')
+plt.plot(epochs, val_acc, label='validation acc')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy-%')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, label='training loss')
+plt.plot(epochs, val_loss, label='validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Binary Crossentropy Loss')
+plt.legend()
+
+train_result = brain_model.evaluate(training_set)
+val_result = brain_model.evaluate(test_set)
